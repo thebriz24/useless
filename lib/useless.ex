da7fbcbd608420e533051664479e62f50af5552a
Brandon Bennett <brandonabennett24@gmail.com>
2018-06-12 14:55:56 -0600

    Typo

--- lib/useless.ex
+++ lib/useless.ex
@@ -1,6 +1,6 @@
 defmodule Useless do
   @moduledoc """
-    Appliction that doesn't do anything except start up a supervisor with no children. For the purpose of having an app that a testing runtime can start and stop with no consequences.
+    Application that doesn't do anything except start up a supervisor with no children. For the purpose of having an app that a testing runtime can start and stop with no consequences.
   """
   use Application
 
